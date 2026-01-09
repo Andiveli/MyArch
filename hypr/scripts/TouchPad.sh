2ee144b3823b6be7904141a22eac3eaabc86b4e5
eSeylaA69 <andyvt0304@gmail.com>
2026-01-08 19:23:37 -0500

    todas mi config

---
+++ hypr/scripts/TouchPad.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
+# For disabling touchpad.
+# Edit the Touchpad_Device on ~/.config/hypr/UserConfigs/Laptops.conf according to your system
+# use hyprctl devices to get your system touchpad device name
+# source https://github.com/hyprwm/Hyprland/discussions/4283?sort=new#discussioncomment-8648109
+
+notif="$HOME/.config/swaync/images/ja.png"
+
+export STATUS_FILE="$XDG_RUNTIME_DIR/touchpad.status"
+
+enable_touchpad() {
+    printf "true" >"$STATUS_FILE"
+    notify-send -u low -i $notif " Enabling" " touchpad"
+    hyprctl keyword '$TOUCHPAD_ENABLED' "true" -r
+    hyprctl keyword cursor:enabled true
+}
+
+disable_touchpad() {
+    printf "false" >"$STATUS_FILE"
+    notify-send -u low -i $notif " Disabling" " touchpad"
+    hyprctl keyword '$TOUCHPAD_ENABLED' "false" -r
+    hyprctl keyword cursor:enabled false
+}
+
+if ! [ -f "$STATUS_FILE" ]; then
+    enable_touchpad
+else
+    if [ $(cat "$STATUS_FILE") = "true" ]; then
+        disable_touchpad
+    elif [ $(cat "$STATUS_FILE") = "false" ]; then
+        enable_touchpad
+    fi
+fi
